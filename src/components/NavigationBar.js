44b173b38bbe28661a5d47a6be3e008db621e9e4
Amr Al-Refae <amrezo94@gmail.com>
2019-05-15 06:51:55 -0400

    Added site logo icon, optimized button look in header

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
+import Logo from "../images/logo.svg";
 
 function NavigationBar() {
   return (
     <div className="container">
       <Navbar bg="default" variant="light">
-        <Navbar.Brand href="#">Amr Al-Refae</Navbar.Brand>
+        <Navbar.Brand href="#">
+          <img
+            src={Logo}
+            width="40"
+            height="40"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+        </Navbar.Brand>
         <Nav className="ml-auto">
           <Nav.Link href="#">Home</Nav.Link>
           <Nav.Link href="#">My Work</Nav.Link>
